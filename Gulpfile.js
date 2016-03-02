3e3e8aeed637650edc6f2d95a8086ee0a01bed1b
Michael Bletzinger <mbletzinger@pavlovmedia.com>
2016-03-02 12:30:21 -0600

    Added Swagger Example

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,4 +3,13 @@
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 
-var tasks = requireDir('node_modules/swf-ng-kit/src/gulp/tasks');
+global.projectOverrides = {
+    title: 'Hello Example',
+    htmlLanguage: 'en',
+    htmlCharset: 'UTF-8',
+    projectVersion: '1.0.0-SNAPSHOT',
+    ngBrowserifyDeps: []
+};
+
+requireDir('node_modules/swf-ng-kit/src/gulp/tasks');
+requireDir('node_modules/swf-swagger-kit/gulp');
